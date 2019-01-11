a756d18b682ee633a5d8bd4f7f9b4158380eff6d
osboxes <osboxes@localhost>
2019-01-11 18:29:38 -0200

    teste

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <form action="dados.php" method="POST">
+            Nome: <input type="Text" name="nome"><br>        
+            Email: <input type="email" name="email"><br>            
+            enviar <input type="submit" name=""enviar><br>                       
+        </form>
+    </body>        
+</html>
